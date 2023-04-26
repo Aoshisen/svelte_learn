b6276be9c4f93323f7e20bb3cda61c323aba209e
aoshien <aoshisen@fanxiapp.com>
2023-04-26 18:04:05 +0800

    feat: 联系grid 布局

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,13 @@
 <main>
   <h1>{message}</h1>
   <Counter />
+
+  <div class="grid">
+    <div class="item">1</div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+    <div class="item">4</div>
+  </div>
 </main>
 
 <style lang="scss" src="./App.scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import App from "./App.svelte";
 import { injectCustomEventWhenSetItem, watchSetItem } from "./utils";
-import { Node, NodeList } from "./utils";
 
 //改变全局的setItem 方法，派发自定义事件
 injectCustomEventWhenSetItem();
@@ -12,25 +11,7 @@ export const app = new App({
   },
 });
 
-export const otherApp = new App({
-  target: document.getElementById("anotherApp") as HTMLDivElement,
-  props: {
-    message: "Another App",
-  },
-});
-
 //注意调用顺序，只有当全局的injectCustomEventWhenSetItem调用之后，自定义的监听事件才会被触发
 watchSetItem((e) => {
   console.log(e, "这是监听自定义事件的触发函数");
 });
-
-const n1 = new Node(1);
-const n2 = new Node(2);
-const n3 = new Node(3);
-
-n1.next = n2;
-n2.next = n3;
-
-const nodeList = new NodeList(n1);
-
-console.log(n1, nodeList, "this is nodeList<<<<<<<<");
