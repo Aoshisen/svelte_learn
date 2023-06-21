6275b30edbe0ed073b6e54d99aa031c572d8c020
aoshien <aoshisen@fanxiapp.com>
2023-06-21 09:26:11 +0800

    refactory: 增加注释

--- src/components/Modal/modal.svelte
+++ src/components/Modal/modal.svelte
@@ -13,21 +13,23 @@
 
 <script lang="ts">
   import { onDestroy } from "svelte";
-
   let currentModal: HTMLElement;
   let visible = false;
   let closeCallback: () => void;
 
+  //打开弹窗的副作用函数
   function openEffect() {
     window.addEventListener("keydown", close);
     document.body.style.overflow = "hidden";
   }
 
+  //关闭弹窗的副作用函数
   function closeEffect() {
     window.removeEventListener("keydown", close);
     document.body.style.overflow = "";
   }
 
+  //定义挂载到modal 的open 函数
   function open(callback: any) {
     closeCallback = callback;
     if (visible) return;
@@ -36,6 +38,7 @@
     document.body.appendChild(currentModal);
   }
 
+  //定义挂载到modal 的close 函数
   function close() {
     if (!visible) return;
     visible = false;
@@ -43,22 +46,24 @@
     closeCallback && closeCallback();
   }
 
+  //挂载到modal上(通过getModal 可以丛外部拿到此modal的示例)
   modal = { open, close };
 
-  onDestroy(() => {
-    closeEffect();
-  });
+  //当弹窗销毁时，调用close 的副作用函数，清除window 上面添加的addEventListener,以及body 的可滑动
+  onDestroy(closeEffect);
+
+  function handleMaskClick() {
+    close();
+  }
 </script>
 
 <div
   id="topModal"
   class:visible
   bind:this={currentModal}
-  on:click|stopPropagation={() => {
-    console.log("topClose");
-    close();
-  }}
+  on:click|stopPropagation={handleMaskClick}
 >
+  <!-- 阻止modal 里面的点击事件冒泡到全局 -->
   <div id="modal" on:click|stopPropagation={() => {}}>
     <div id="modal-content">
       <slot />
