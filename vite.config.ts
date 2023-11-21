cbea2ce229aa5c2fefce8366919a4a4ba3fffb81
aoshien <aoshisen@fanxiapp.com>
2023-11-21 10:58:31 +0800

    feat: 打包后的文件的banner 添加

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,25 @@ import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { imagetools } from "vite-imagetools";
-import path from "path";
 import progress from "vite-plugin-progress";
+import banner from "vite-plugin-banner";
+import pkg from "./package.json";
+import path from "path";
 
 const filePath = path.dirname(import.meta.url);
-
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
       progress(),
       tsconfigPaths(),
-      //处理图片的插件
-      imagetools(),
+      banner(
+        `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`
+      ),
       svelte({
         preprocess: sveltePreprocess({
           scss: {
-            prependData: `@import "${filePath}/src/assets/global.scss";`,
+            prependData: `@import "${filePath}/src/assets/styles/global.scss";`,
           },
         }),
         onwarn(warning, defaultHandler) {
